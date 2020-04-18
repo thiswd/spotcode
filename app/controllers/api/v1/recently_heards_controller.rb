19f63e7dcd778bc7d9d7e3abfa638ef1fc5d7285
Thiago <tms1_tms2@hotmail.com>
2020-04-18 13:26:13 -0300

    Backend

---
+++ app/controllers/api/v1/recently_heards_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::RecentlyHeardsController < ApplicationController
+  def create
+    @recently_heard = current_user
+      .recently_heards
+      .create(album_id: params[:album_id])
+    head :ok
+  end
+end
